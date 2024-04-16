a33ec438f194537b824c3633a4532936cf8b5806
Zain <mzain9@gatech.edu>
2024-04-16 14:46:31 -0400

    added scripts

--- script.sh
+++ script.sh
@@ -3,13 +3,14 @@ echo "Running the executable"
 echo "delete the previous txt file"
 rm output_of_mean.txt
 rm file_*.txt
+cam_size=64
+num_sub_cams=8
 # Of the form CAM SIZE, Number of Sub CAMS and Range of LNREGs
 # write a loop to run the executable for different values
 echo "Running the executable for different values"
-echo "CAM SIZE: 32, Number of Sub CAMS: 8, Range of LNREGs: 128"
 for i in 2 4 8 16
 do  
     echo "Running for $i"
-    ./build/RSCA_executable 32 8 $((32*$i)) >> file_$i.txt
+    ./build/RSCA_executable $cam_size $num_sub_cams $(($cam_size*$i)) >> file_$i.txt
 done
 echo "Saved in file.txt in the parent folder"
